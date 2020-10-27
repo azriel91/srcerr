5772af78719ef4b2c3780bba31522358b4fceba3
Azriel Hoh <azriel91@gmail.com>
2020-10-27 19:46:41 +1300

    Rename `RelevantSource` to `SourceHighlighted`.

--- src/source_error.rs
+++ src/source_error.rs
@@ -1,10 +1,10 @@
-use crate::{RelevantSource, Severity, Suggestion};
+use crate::{Severity, SourceHighlighted, Suggestion};
 
 /// Information about an error from source data.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct SourceError<'path, 'source> {
     /// Source data that the erroneous value is found.
-    pub invalid_source: RelevantSource<'path, 'source>,
+    pub invalid_source: SourceHighlighted<'path, 'source>,
     /// Suggestion or hint to provide to the user.
     pub suggestion: Option<Suggestion<'source>>,
     /// Whether this is a denied error or warning.
--- src/source_highlighted.rs
+++ src/source_highlighted.rs
@@ -4,7 +4,7 @@ use crate::Expr;
 
 /// Relevant part of source data to highlight to the user.
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct RelevantSource<'path, 'source> {
+pub struct SourceHighlighted<'path, 'source> {
     /// Path to the source that the source data comes from.
     pub path: &'path Path,
     /// Expression that should be highlighted.
