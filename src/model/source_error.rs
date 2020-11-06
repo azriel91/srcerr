eac5e16a359da5c43db78bf9a0d724024dad7c7b
Azriel Hoh <azriel91@gmail.com>
2020-11-07 00:02:24 +1300

    Add `error_code` field to `SourceError`.

--- src/model/source_error.rs
+++ src/model/source_error.rs
@@ -1,8 +1,14 @@
 use crate::{Severity, SourceHighlighted, Suggestion};
 
 /// Information about an error from source data.
+///
+/// # Type Parameters
+///
+/// * `E`: [`ErrorCode`] type.
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct SourceError<'path, 'source> {
+pub struct SourceError<'path, 'source, E> {
+    /// Code within the [`ErrorCode`] this error corresponds to.
+    pub error_code: E,
     /// Source data that the erroneous value is found.
     pub invalid_source: SourceHighlighted<'path, 'source>,
     /// Suggestions or hints to provide to the user.
