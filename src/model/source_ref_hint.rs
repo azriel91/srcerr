e226564adb5aed03fa62350cd522ad54567f1a13
Azriel Hoh <azriel91@gmail.com>
2020-11-10 15:55:22 +1300

    Remove `Severity` from `SourceRefHint`.

--- src/model/source_ref_hint.rs
+++ src/model/source_ref_hint.rs
@@ -1,12 +1,10 @@
-use crate::{Severity, SourceHighlighted};
+use crate::SourceHighlighted;
 
 /// Reference a different part of the source data as a hint.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct SourceRefHint<'path, 'source> {
     /// Source data that the hint appears in.
     pub source_ref: SourceHighlighted<'path, 'source>,
-    /// Whether this is a denied error or warning.
-    pub severity: Severity,
     /// Description that accompanies the highlighted source.
     ///
     /// This is printed alongside the error in the following format:
