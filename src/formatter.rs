bdac4bb507df9fa4954b1ff913edd564aa712ab0
Azriel Hoh <azriel91@gmail.com>
2020-11-19 20:29:07 +1300

    Gate `AnsiColorFormatter` behind `"ansi_color"` feature.

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,11 +1,16 @@
 //! Formatters for source errors.
 
 pub use self::{
-    ansi_color_formatter::AnsiColorFormatter, plain_text_formatter::PlainTextFormatter,
-    source_error_formatter::SourceErrorFormatter, styler::Styler,
+    plain_text_formatter::PlainTextFormatter, source_error_formatter::SourceErrorFormatter,
+    styler::Styler,
 };
 
-mod ansi_color_formatter;
 mod plain_text_formatter;
 mod source_error_formatter;
 mod styler;
+
+#[cfg(feature = "ansi_color")]
+pub use self::ansi_color_formatter::AnsiColorFormatter;
+
+#[cfg(feature = "ansi_color")]
+mod ansi_color_formatter;
